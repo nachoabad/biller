bc8e4fe01b49b1ea12cbcd7d1a9b974c0bf1e424
Nacho Abad <liao2512@gmail.com>
2019-04-21 08:17:15 +0000

    adds Bill and LineItem methods, associations and validations

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,3 +1,40 @@
 class Bill < ApplicationRecord
+  OVERDUE_DAILY_CHARGE = 5
+
   belongs_to :client
+  has_many   :line_items, dependent: :destroy
+
+  validates :due_date, presence: true
+
+  def total
+    line_items.sum(&:total) + additional_charges
+  end
+
+  def additional_charges
+    days_past_due * OVERDUE_DAILY_CHARGE
+  end
+
+  private
+
+  def days_past_due
+    if currently_overdue?
+      (Date.current - due_date).to_i
+    elsif paid_overdue?
+      (paid_date - due_date).to_i
+    else
+      0
+    end
+  end
+
+  def paid_overdue?
+    paid? && (paid_date > due_date)
+  end
+
+  def currently_overdue?
+    !paid? && (Date.current > due_date)
+  end
+
+  def paid?
+    paid_date.present?
+  end
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,30 @@
 class LineItem < ApplicationRecord
+  include Taxable
+
   belongs_to :item
   belongs_to :bill
+
+  delegate :name, :type, :price, to: :item
+
+  def sub_total
+    item.price * quantity
+  end
+
+  def tax
+    item.taxable? ? sub_total * tax_rate : 0
+  end
+
+  def total
+    sub_total + tax
+  end
+
+  private
+
+  def tax_rate
+    applicable_tax(client.postal_code)
+  end
+
+  def client
+    bill.client
+  end
 end
