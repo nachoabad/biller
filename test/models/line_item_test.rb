29d9a3f12b6b9bdca101b8a2f969a04728517165
Nacho Abad <liao2512@gmail.com>
2019-04-21 10:01:24 +0000

    adds line item tax unit test

--- test/models/line_item_test.rb
+++ test/models/line_item_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class LineItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "tax" do
+    assert_equal 3.597, line_items(:due_one).tax
+    assert_equal 0, line_items(:due_three).tax
+    assert_equal 20.691, line_items(:currently_overdue_two).tax
+    assert_equal 0, line_items(:currently_overdue_four).tax
+    assert_equal 2.398, line_items(:paid_overdue_one).tax
+    assert_equal 0, line_items(:paid_overdue_three).tax
+    assert_equal 13.794, line_items(:paid_ontime_two).tax
+    assert_equal 0, line_items(:paid_ontime_four).tax
+  end
 end
