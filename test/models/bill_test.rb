e2d1b4708bc665f952fbfaf238102a469d69691f
Nacho Abad <liao2512@gmail.com>
2019-04-21 10:02:02 +0000

    adds bill unit tests

--- test/models/bill_test.rb
+++ test/models/bill_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class BillTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "additional_charges" do
+    assert_equal 0, bills(:due).additional_charges
+    assert_equal 5, bills(:currently_overdue).additional_charges
+    assert_equal 5, bills(:paid_overdue).additional_charges
+    assert_equal 0, bills(:paid_ontime).additional_charges
+  end
+  
+  test "total" do
+    assert_equal 39.567, bills(:due).total
+    assert_equal 186.621, bills(:currently_overdue).total
+    assert_equal 43.368, bills(:paid_overdue).total
+    assert_equal 174.724, bills(:paid_ontime).total
+  end
 end
