46e7af166ff4c5df981b7b5348d3c06a9b61ce27
Nacho Abad <liao2512@gmail.com>
2019-04-21 07:54:03 +0000

    adds Client associations and validations

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,7 @@
 class Client < ApplicationRecord
+  has_many :bills, dependent: :destroy
+
+  validates :name, :postal_code, presence: true
+  validates :postal_code, format: { with: /\A[+-]?\d+\z/,
+                                    message: "%{value} is not a valid postal code" }
 end
