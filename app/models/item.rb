8c7f99d75a013d05a5b2cbb161d4c3e4e82489e3
Nacho Abad <liao2512@gmail.com>
2019-04-21 07:59:19 +0000

    adds Item associations and validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  has_many   :line_items, dependent: :destroy
+
+  validates :name, :type, :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :type, inclusion: { in: %w(Product Service),
+                                message: "%{value} is not a valid type" }
 end
