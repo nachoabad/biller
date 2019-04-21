66c31c5505f6e98fbec680a66cb4e6f68e5838d7
Nacho Abad <liao2512@gmail.com>
2019-04-21 09:11:45 +0000

    routes products and services resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :line_items
   resources :bills
-  resources :items
+  resources :products
+  resources :services
   resources :clients
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
